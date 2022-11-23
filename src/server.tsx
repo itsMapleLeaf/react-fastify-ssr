98ef1fa669f85cad7046f21b638696d44d7f6310
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-11-23 12:26:46 -0600

    hi prime i love you 💖

---
+++ src/server.tsx
@@ -0,0 +1,28 @@
+import Fastify from "fastify"
+import { renderToString } from "react-dom/server"
+
+const fastify = Fastify({
+  logger: true,
+})
+
+fastify.get("/", function (request, reply) {
+  const rendered = renderToString(
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>hi</title>
+      </head>
+      <body>the</body>
+    </html>,
+  )
+  reply.header("Content-Type", "text/html").send(rendered)
+})
+
+fastify.listen({ port: 3000 }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+})
